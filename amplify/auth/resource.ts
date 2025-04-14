76b63b0ae94d3d7dd17337b8c75fc834dbf9c777
Ion Girloanta <ion_g@yahoo.com>
2025-04-14 09:49:07 +0300

    Update modified files

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -20,5 +20,4 @@ export const auth = defineAuth({
       //dialCode: '+972'
     }
   }
-
 });
--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,47 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import { Authenticator } from '@aws-amplify/ui-react'; //import AmplifyUI from amplify ui-react;
 import '@aws-amplify/ui-react/styles.css'; //import AmplifyUI styleSheet from amplify ui-react;
-
+import { View, Image, Text, useTheme } from '@aws-amplify/ui-react';
 const formFields = {
   signIn: {
     username: {
-      dialCode: '+227'
+      dialCode: '+972'
     },
   },
   signUp: {
     phone_number: {
-      dialCode: '+227'
+      dialCode: '+972'
     },
   },
 }
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="Amplify logo"
+          //src="https://docs.amplify.aws/assets/logo-dark.svg"
+          src="https://israelxp.com/wp-content/uploads/2020/11/logo-bar-ilan-university.png"
+          width="50%"
+        />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+};
 
 
 Amplify.configure(outputs);
@@ -26,7 +54,7 @@ Amplify.configure(outputs);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   
   <React.StrictMode>
-    <Authenticator  formFields={formFields}>
+    <Authenticator  formFields={formFields} components={components}>
       <App />
     </Authenticator>
   </React.StrictMode>);
